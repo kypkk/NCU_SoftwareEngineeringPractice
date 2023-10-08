4d6d1c2137aa8c473d0cea166874a666027fa59c
kypkk <kypss50102@gmail.com>
2023-10-08 18:21:24 +0800

    add: HW6 FilterSystem test

--- Homework 6/src/core/FilterSystem.test.ts
+++ Homework 6/src/core/FilterSystem.test.ts
@@ -1,5 +1,45 @@
-describe("Need implement", () => {
-    test("Todo", () => {
-        expect(1).toEqual(1);
-    });
+import { FilterSystem } from "./FilterSystem";
+import { TestBookInfo } from "../__test__/TestingData";
+
+describe("Test Filter System", () => {
+  let system = new FilterSystem();
+  system.setFilterWord("To");
+  system.setIgnoreCase(true);
+  system.process(TestBookInfo);
+
+  test("Tests:\n\t1. FilterSystem.setFilterWord('To')\n\t2. FilterSystem.IgnoreCases(true)\n\t3. FilterSystem.process\n\t4. BaseSystem.getItems", () => {
+    expect(system.getItems()).toStrictEqual([
+      {
+        ISBN: "774-13-13326-60-1",
+        title: "To Kill a Mockingbird",
+        author: "Danielle Steel",
+      },
+      {
+        ISBN: "572-70-62221-82-2",
+        title: "Emma Story",
+        author: "Henry James",
+      },
+    ]);
+  });
+  test("Test:\n\tFilterSystem.isIgnoreCase() == true", () => {
+    expect(system.isIgnoreCase()).toStrictEqual(true);
+  });
+  test("Test:\n\tFilterSystem.FilterWord() == 'To'", () => {
+    expect(system.getFilterWord()).toStrictEqual("To");
+  });
+
+  test("Test:\n\tWhen ignore case is false then TestBookInfo should have only one object", () => {
+    system.setIgnoreCase(false);
+    system.process(TestBookInfo);
+    expect(system.getItems()).toStrictEqual([
+      {
+        ISBN: "774-13-13326-60-1",
+        title: "To Kill a Mockingbird",
+        author: "Danielle Steel",
+      },
+    ]);
+  });
+  test("Test:\n\tBaseSystem.getUpdateMessage() == Filter Update", () => {
+    expect(system.getUpdateMessage()).toStrictEqual("Filter Update");
+  });
 });
