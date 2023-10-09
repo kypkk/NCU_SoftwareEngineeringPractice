d00224c7411c1d71b91cac7b1c6957f9c00b8d05
kypkk <kypss50102@gmail.com>
2023-10-09 14:25:21 +0800

    add: HashGenerator test

--- Homework 6/src/utils/HashGenerator.test.ts
+++ Homework 6/src/utils/HashGenerator.test.ts
@@ -1,5 +1,41 @@
-describe("Need implement", () => {
-    test("Todo", () => {
-        expect(1).toEqual(1);
-    });
+import { HashGenerator } from "./HashGenerator";
+describe("Test for the Hash Generator", () => {
+  const generator = new HashGenerator();
+  const mockMath = Object.create(global.Math);
+  mockMath.random = () => 0.97;
+  global.Math = mockMath;
+
+  /**
+   * Tests:
+   * - HashGenerator.g()
+   */
+  test("Tests:\n\tgenerator", () => {
+    let randomGenString = generator.g(1);
+    expect(randomGenString).toBe("Z");
+  });
+  /**
+   * Tests:
+   * - Error call g
+   */
+  test("Tests\n\tError call generation", () => {
+    const randomGenString = (): string => {
+      return generator.g(0);
+    };
+
+    expect(randomGenString).toThrow("Hash number can't less than 0");
+  });
+  /**
+   * Tests:
+   * - HashGenerator.simpleISBN()
+   */
+  test("Tests:\n\tsimpleISBN", () => {
+    let simpleISBN = generator.simpleISBN("xxxx-xxxx-xxxx-xxxx");
+    expect(simpleISBN).toBe("9999-9999-9999-9999");
+    simpleISBN = generator.simpleISBN("");
+    expect(simpleISBN).toBe("");
+    simpleISBN = generator.simpleISBN("-");
+    expect(simpleISBN).toBe("-");
+    simpleISBN = generator.simpleISBN("f");
+    expect(simpleISBN).toBe("9");
+  });
 });
